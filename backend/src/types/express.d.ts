e0defa1692bb83578ee4344a06de5fa76650fb0e
ayushks404 <ayushks9415@gmail.com>
2026-05-05 00:39:08 +0530

    adding auths endpoints

---
+++ backend/src/types/express.d.ts
@@ -0,0 +1,12 @@
+import { ProjectMember } from '@prisma/client';
+
+declare global {
+  namespace Express {
+    interface Request {
+      user?: { id: string; email: string; name: string };
+      membership?: ProjectMember;
+    }
+  }
+}
+
+export {};
